b99477c790debdb77b4c34606bee2eebfdca205a
prashant7090 <prashant7090@gmail.com>
2017-05-07 00:36:45 +0530

    Added POJO for customer.

---
+++ complete/src/main/java/com/retailmanager/model/Customer.java
@@ -0,0 +1,25 @@
+package com.retailmanager.model;
+
+/**
+ * Created by prashant on 5/6/17.
+ */
+public class Customer {
+    private double latitude;
+    private double longitude;
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}

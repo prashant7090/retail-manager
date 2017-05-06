c3f47919c4175ad7a1b8fdeb13e384ac556650d3
prashant7090 <prashant7090@gmail.com>
2017-05-07 01:48:41 +0530

    Added method documentation for shopDaoImpl.

--- complete/src/main/java/com/retailmanager/impl/ShopDaoImpl.java
+++ complete/src/main/java/com/retailmanager/impl/ShopDaoImpl.java
@@ -24,6 +24,13 @@ public class ShopDaoImpl implements ShopDao{
     @Autowired
     Haversine haversine;
 
+    /**
+     * Returns true if shop is added or false if updated. More formally, The shop name is unique
+     * so, if another/same user tries to add shop with the same name, we update the shop info.
+     * @param shop shop to be added .
+     * @return <tt>true</tt> if new shop is added
+     * @return <tt>false</tt> if shop information is updated
+     */
     @Override
     public boolean addShop(Shop shop) {
         for (;;) {
@@ -38,9 +45,16 @@ public class ShopDaoImpl implements ShopDao{
 
     }
 
+    /**
+     * Returns all the near by shops within NEARBYDISTANCE. More formally, It finds the shops which are in
+     * 10KM radios.  The distance is calculated by haversine formula by
+     * specifying customer's latitude, customer's longitude, shop's latitude and shop's longitude.
+     * @param latitude Customer's latitude.
+     * @param longitude Customer's longitude.
+     * @return <tt>List if Shops </tt>
+     */
     @Override
     public List<Shop> getNearByShops(Double latitude, Double longitude) {
-
         return shopMap.entrySet().stream()
                 .filter(shops -> isValidLatLong(shops.getValue().getLat(), shops.getValue().getLng()))
                 .filter(validShop ->
@@ -54,12 +68,26 @@ public class ShopDaoImpl implements ShopDao{
 
     }
 
+    /**
+     * Returns <tt>true</tt> if distance is less than or equals
+     * to NEARBYDISTANCE.
+     * @param distance distance to be tested for nearby
+     * @return <tt>true</tt> if distance is less than or equals
+     * to NEARBYDISTANCE.
+     */
     private boolean isNearByDistance(double distance){
         return distance <= NEARBYDISTANCE;
     }
 
 
-
+    /**
+     * Returns <tt>true</tt> if this latitude & longitude is parsable to
+     * double, More Formally, Some shop may not have latitude and longitude.
+     * @param latitude latitude to test parsable to double.
+     * @param longitude longitude to test parsable to double.
+     * @return <tt>true</tt> if parameters are parsable to
+     * double
+     */
     private boolean isValidLatLong(String latitude, String longitude){
         try{
             Double.parseDouble(latitude);
